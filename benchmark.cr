e25518e18544c38223d858504ede8790c70b6997
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-07-23 17:36:29 +0300

    refactor

--- benchmark.cr
+++ benchmark.cr
@@ -8,6 +8,8 @@ opts = Sophia::H{"compression"      => "zstd",
 chest = Trove::Chest.new Trove::Env.new Sophia::H{"sophia.path" => "/tmp/trove"}, {d: opts, i: opts}
 
 cs = JSON.parse COMPLEX_STRUCTURE.to_json
+k = "level1.level2.level3.1.metadata.level4.level5.level6.note"
+v = "This is six levels deep"
 
 Benchmark.ips do |b|
   b.report "set+delete" do
@@ -17,30 +19,30 @@ end
 i = chest << cs
 Benchmark.ips do |b|
   b.report "has key" do
-    raise "Can not get" unless chest.has_key? i, "level1.level2.level3.1.metadata.level4.level5.level6.note"
+    raise "Can not get" unless chest.has_key? i, k
   end
   b.report "has key (only simple)" do
-    raise "Can not get" unless chest.has_key! i, "level1.level2.level3.1.metadata.level4.level5.level6.note"
+    raise "Can not get" unless chest.has_key! i, k
   end
   b.report "get full" do
     raise "Can not get" if chest.get(i) != cs
   end
   b.report "get field" do
-    raise "Can not get" if chest.get(i, "level1.level2.level3.1.metadata.level4.level5.level6.note") != "This is six levels deep"
+    raise "Can not get" if chest.get(i, k) != v
   end
   b.report "get field (only simple)" do
-    raise "Can not get" if chest.get!(i, "level1.level2.level3.1.metadata.level4.level5.level6.note") != "This is six levels deep"
+    raise "Can not get" if chest.get!(i, k) != v
   end
 end
 Benchmark.ips do |b|
   n = 10**4 - 1
   (1..n).each { chest << cs }
   b.report "get one oid from index" do
-    chest.where!("level1.level2.level3.1.metadata.level4.level5.level6.note", "This is six levels deep") { |ii| break }
+    chest.where!(k, v) { |ii| break }
   end
   b.report "get #{n + 1} oids from index" do
     g = 0
-    chest.where!("level1.level2.level3.1.metadata.level4.level5.level6.note", "This is six levels deep") { |ii| g += 1 }
+    chest.where!(k, v) { |ii| g += 1 }
     raise "#{g} != #{n + 1}" if g != n + 1
   end
 end
