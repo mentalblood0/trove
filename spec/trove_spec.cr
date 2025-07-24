d0f1bcc7ef2a72ab008efb85c9d1cfe3a2170090
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-07-24 15:07:59 +0300

    more intuitive name

--- spec/trove_spec.cr
+++ spec/trove_spec.cr
@@ -10,7 +10,7 @@ describe Trove do
 
   it "example" do
     parsed = JSON.parse %({
-                            "string": {
+                            "dict": {
                               "hello": ["number", 42, -4.2, 0.0],
                               "boolean": false
                             },
@@ -20,20 +20,20 @@ describe Trove do
     oid = chest << parsed
 
     chest.get(oid).should eq parsed
-    chest.get(oid, "string").should eq({"hello" => ["number", 42, -4.2, 0.0], "boolean" => false})
-    chest.get(oid, "string.hello").should eq ["number", 42, -4.2, 0.0]
-    chest.get(oid, "string.boolean").should eq false
+    chest.get(oid, "dict").should eq({"hello" => ["number", 42, -4.2, 0.0], "boolean" => false})
+    chest.get(oid, "dict.hello").should eq ["number", 42, -4.2, 0.0]
+    chest.get(oid, "dict.boolean").should eq false
 
     # get! is faster than get, but expects simple value
     # because under the hood all values in trove are simple
     # and get! just gets value by key, without range scan
 
-    chest.get!(oid, "string.boolean").should eq false
-    chest.get!(oid, "string").should eq nil
-    chest.get!(oid, "string.hello.0").should eq "number"
-    chest.get!(oid, "string.hello.1").should eq 42
-    chest.get!(oid, "string.hello.2").should eq -4.2
-    chest.get!(oid, "string.hello.3").should eq 0.0
+    chest.get!(oid, "dict.boolean").should eq false
+    chest.get!(oid, "dict").should eq nil
+    chest.get!(oid, "dict.hello.0").should eq "number"
+    chest.get!(oid, "dict.hello.1").should eq 42
+    chest.get!(oid, "dict.hello.2").should eq -4.2
+    chest.get!(oid, "dict.hello.3").should eq 0.0
     chest.get!(oid, "null").should eq nil
     chest.get!(oid, "nonexistent.key").should eq nil
     chest.get(oid, "array").should eq [1, ["two", false], [nil]]
@@ -46,72 +46,72 @@ describe Trove do
 
     chest.has_key?(oid, "null").should eq true
     chest.has_key!(oid, "null").should eq true
-    chest.has_key?(oid, "string").should eq true
-    chest.has_key!(oid, "string").should eq false
+    chest.has_key?(oid, "dict").should eq true
+    chest.has_key!(oid, "dict").should eq false
     chest.has_key?(oid, "nonexistent.key").should eq false
     chest.has_key!(oid, "nonexistent.key").should eq false
 
     # indexes work for simple values
 
     oids = [] of Trove::Oid
-    chest.where!("string.boolean", false) { |i| oids << i }
+    chest.where!("dict.boolean", false) { |i| oids << i }
     oids.should eq [oid]
 
-    chest.where!("string.boolean", true) { |i| raise "Who is here?" }
+    chest.where!("dict.boolean", true) { |i| raise "Who is here?" }
 
     oids = [] of Trove::Oid
-    chest.where!("string.hello.0", "number") { |i| oids << i }
+    chest.where!("dict.hello.0", "number") { |i| oids << i }
     oids.should eq [oid]
 
     # where! looks for exact path match and where only for beginning of,
     # so where is useful for arrays elements for example
 
-    chest.where!("string.hello", "number") { |i| raise "Who is here?" }
+    chest.where!("dict.hello", "number") { |i| raise "Who is here?" }
     oids = [] of Trove::Oid
-    chest.where("string.hello", "number") { |i| oids << i }
+    chest.where("dict.hello", "number") { |i| oids << i }
     oids.should eq [oid]
 
     # unique is way faster than where,
     # but works correctly only for values that were always unique
 
-    chest.unique("string.boolean", false).should eq oid
+    chest.unique("dict.boolean", false).should eq oid
 
-    chest.delete! oid, "string.hello"
-    chest.get(oid, "string.hello").should eq ["number", 42, -4.2, 0.0]
+    chest.delete! oid, "dict.hello"
+    chest.get(oid, "dict.hello").should eq ["number", 42, -4.2, 0.0]
 
-    chest.delete! oid, "string.hello.2"
-    chest.get(oid, "string.hello.2").should eq nil
-    chest.get(oid, "string.hello").should eq ["number", 42, 0.0]
+    chest.delete! oid, "dict.hello.2"
+    chest.get(oid, "dict.hello.2").should eq nil
+    chest.get(oid, "dict.hello").should eq ["number", 42, 0.0]
 
-    chest.delete oid, "string.hello"
-    chest.get(oid, "string.hello").should eq nil
-    chest.get(oid, "string").should eq({"boolean" => false})
+    chest.delete oid, "dict.hello"
+    chest.get(oid, "dict.hello").should eq nil
+    chest.get(oid, "dict").should eq({"boolean" => false})
 
-    chest.delete! oid, "string.boolean"
-    chest.where!("string.boolean", false) { |i| raise "Who is here?" }
-    chest.get(oid, "string").should eq nil
+    chest.delete! oid, "dict.boolean"
+    chest.where!("dict.boolean", false) { |i| raise "Who is here?" }
+    chest.get(oid, "dict").should eq nil
     chest.get(oid).should eq({"null" => nil, "array" => [1, ["two", false], [nil]]})
 
-    chest.set oid, "string", parsed["string"]
-    chest.get(oid, "string").should eq parsed["string"]
-    chest.set oid, "string.boolean", JSON.parse %({"a": "b", "c": 4})
-    chest.get(oid, "string.boolean").should eq({"a" => "b", "c" => 4})
+    chest.set oid, "dict", parsed["dict"]
+    chest.get(oid, "dict").should eq parsed["dict"]
+    chest.set oid, "dict.boolean", JSON.parse %({"a": "b", "c": 4})
+    chest.get(oid, "dict.boolean").should eq({"a" => "b", "c" => 4})
 
     # set! works when overwriting simple values
 
-    chest.set! oid, "string.null", parsed["array"]
-    chest.get(oid, "string.null").should eq parsed["array"]
+    chest.set! oid, "dict.null", parsed["array"]
+    chest.get(oid, "dict.null").should eq parsed["array"]
 
-    s = chest.get(oid, "string").not_nil!
+    s = chest.get(oid, "dict").not_nil!
     begin
       chest.transaction do |tx|
-        tx.delete oid, "string"
+        tx.delete oid, "dict"
         raise "oh no"
         tx << s
       end
     rescue ex
       ex.message.should eq "oh no"
-      chest.get(oid, "string").should eq s
+      chest.get(oid, "dict").should eq s
     end
 
     chest.delete oid
@@ -122,8 +122,8 @@ describe Trove do
     chest.delete oid
     chest.get(oid).should eq nil
     chest.get(oid, "null").should eq nil
-    chest.where!("string.boolean", false) { |i| raise "Who is here?" }
-    chest.where("string", false) { |i| raise "Who is here?" }
+    chest.where!("dict.boolean", false) { |i| raise "Who is here?" }
+    chest.where("dict", false) { |i| raise "Who is here?" }
   end
 
   [
