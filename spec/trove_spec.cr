7b702146fed2799145e0a57d7c2a92156b6a2699
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-07-30 18:50:38 +0300

    use xxhash for indexes

--- spec/trove_spec.cr
+++ spec/trove_spec.cr
@@ -56,22 +56,18 @@ describe Trove do
     chest.oids { |i| all_oids << i }
     all_oids.should eq [oid]
 
-    # indexes work for simple values
+    # indexes work for simple values as well as for arrays
 
     oids = [] of Trove::Oid
-    chest.where!("dict.boolean", false) { |i| oids << i }
+    chest.where("dict.boolean", false) { |i| oids << i }
     oids.should eq [oid]
 
-    chest.where!("dict.boolean", true) { |i| raise "Who is here?" }
+    chest.where("dict.boolean", true) { |i| raise "Who is here?" }
 
     oids = [] of Trove::Oid
-    chest.where!("dict.hello.0", "number") { |i| oids << i }
+    chest.where("dict.hello.0", "number") { |i| oids << i }
     oids.should eq [oid]
 
-    # where! looks for exact path match and where only for beginning of,
-    # so where is useful for arrays elements for example
-
-    chest.where!("dict.hello", "number") { |i| raise "Who is here?" }
     oids = [] of Trove::Oid
     chest.where("dict.hello", "number") { |i| oids << i }
     oids.should eq [oid]
@@ -93,7 +89,7 @@ describe Trove do
     chest.get(oid, "dict").should eq({"boolean" => false})
 
     chest.delete! oid, "dict.boolean"
-    chest.where!("dict.boolean", false) { |i| raise "Who is here?" }
+    chest.where("dict.boolean", false) { |i| raise "Who is here?" }
     chest.get(oid, "dict").should eq nil
     chest.get(oid).should eq({"null" => nil, "array" => [1, ["two", false], [nil]]})
 
@@ -132,7 +128,7 @@ describe Trove do
     chest.delete oid
     chest.get(oid).should eq nil
     chest.get(oid, "null").should eq nil
-    chest.where!("dict.boolean", false) { |i| raise "Who is here?" }
+    chest.where("dict.boolean", false) { |i| raise "Who is here?" }
     chest.where("dict", false) { |i| raise "Who is here?" }
     chest.oids { raise "Who is here?" }
   end
@@ -169,61 +165,61 @@ describe Trove do
     end
   end
 
-  # [
-  #   "string",
-  #   1234_i64,
-  #   1234.1234_f64,
-  #   -1234_i64,
-  #   -1234.1234_f64,
-  #   0_i64,
-  #   0.0_f64,
-  #   true,
-  #   false,
-  #   nil,
-  #   {"key" => "value"},
-  #   {"a" => "b", "c" => "d"},
-  #   {"a" => {"b" => "c"}},
-  #   {"a" => {"b" => {"c" => "d"}}},
-  #   {"a" => {"b" => {"c" => "d"}}},
-  #   ["a", "b", "c"],
-  #   ["a"],
-  #   [1_i64, 2_i64, 3_i64],
-  #   [1_i64],
-  #   ["a", 1_i64, true, 0.0_f64],
-  #   COMPLEX_STRUCTURE,
-  # ].each do |o|
-  #   it "add+get+where+delete #{o}" do
-  #     j = o.to_json
-  #     p = JSON.parse j
-  #     i = chest << p
-  #     chest.get(i).should eq o
+  [
+    "string",
+    1234_i64,
+    1234.1234_f64,
+    -1234_i64,
+    -1234.1234_f64,
+    0_i64,
+    0.0_f64,
+    true,
+    false,
+    nil,
+    {"key" => "value"},
+    {"a" => "b", "c" => "d"},
+    {"a" => {"b" => "c"}},
+    {"a" => {"b" => {"c" => "d"}}},
+    {"a" => {"b" => {"c" => "d"}}},
+    ["a", "b", "c"],
+    ["a"],
+    [1_i64, 2_i64, 3_i64],
+    [1_i64],
+    ["a", 1_i64, true, 0.0_f64],
+    COMPLEX_STRUCTURE,
+  ].each do |o|
+    it "add+get+where+delete #{o}" do
+      j = o.to_json
+      p = JSON.parse j
+      i = chest << p
+      chest.get(i).should eq o
 
-  #     chest.has_key?(i).should eq true
+      chest.has_key?(i).should eq true
 
-  #     case o
-  #     when String, Int64, Float64, Bool, Nil
-  #       chest.where!("", o) { |ii| ii.should eq i }
-  #       chest.has_key!(i).should eq true
-  #     when Array
-  #       o.each_with_index do |v, k|
-  #         chest.has_key!(i, k.to_s).should eq true
-  #         chest.where!(k.to_s, v) { |ii| ii.should eq i }
-  #       end
-  #     when Hash(String, String)
-  #       o.each do |k, v|
-  #         chest.has_key!(i, k).should eq true
-  #         chest.where!(k.to_s, v) { |ii| ii.should eq i }
-  #       end
-  #     when COMPLEX_STRUCTURE
-  #       chest.has_key!(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq true
-  #       chest.get(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq "This is six levels deep"
-  #       chest.get!(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq "This is six levels deep"
-  #       chest.where!("level1.level2.level3.1.metadata.level4.level5.level6.note", "This is six levels deep") { |ii| break }
-  #     end
+      case o
+      when String, Int64, Float64, Bool, Nil
+        chest.where("", o) { |ii| ii.should eq i }
+        chest.has_key!(i).should eq true
+      when Array
+        o.each_with_index do |v, k|
+          chest.has_key!(i, k.to_s).should eq true
+          chest.where(k.to_s, v) { |ii| ii.should eq i }
+        end
+      when Hash(String, String)
+        o.each do |k, v|
+          chest.has_key!(i, k).should eq true
+          chest.where(k.to_s, v) { |ii| ii.should eq i }
+        end
+      when COMPLEX_STRUCTURE
+        chest.has_key!(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq true
+        chest.get(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq "This is six levels deep"
+        chest.get!(i, "level1.level2.level3.1.metadata.level4.level5.level6.note").should eq "This is six levels deep"
+        chest.where("level1.level2.level3.1.metadata.level4.level5.level6.note", "This is six levels deep") { |ii| break }
+      end
 
-  #     chest.delete i
-  #     chest.has_key!(i).should eq false
-  #     chest.get(i).should eq nil
-  #   end
-  # end
+      chest.delete i
+      chest.has_key!(i).should eq false
+      chest.get(i).should eq nil
+    end
+  end
 end
--- src/trove.cr
+++ src/trove.cr
@@ -22,12 +22,13 @@ module Trove
                                     di1: UInt64,
                                     dp: String},
                               value: {dv: String}},
-                          i: {key: {ip: String,
-                                    iv: String,
+                          i: {key: {ipv0: UInt64,
+                                    ipv1: UInt64,
+                                    ipi: UInt32,
                                     ii0: UInt64,
                                     ii1: UInt64}},
-                          u: {key: {up: String,
-                                    uv: String},
+                          u: {key: {upv0: UInt64,
+                                    upv1: UInt64},
                               value: {ui0: UInt64,
                                       ui1: UInt64}},
                           o: {key: {oi0: UInt64,
@@ -78,6 +79,11 @@ module Trove
       end
     end
 
+    protected def partition(p : String)
+      pp = p.rpartition '.'
+      {b: pp[0], i: pp[2].to_u32} rescue {b: p, i: 0_u32}
+    end
+
     protected def set(i : Oid, p : String, o : A::Type)
       case o
       when H
@@ -93,9 +99,10 @@ module Trove
         oe = encode o
       end
       @env << {di0: i[0], di1: i[1], dp: p, dv: oe}
-      iv = index_value p.bytesize + 16, oe
-      @env << {ip: p, iv: iv, ii0: i[0], ii1: i[1]}
-      @env << {up: p, uv: iv, ui0: i[0], ui1: i[1]}
+      pp = partition p
+      d = digest (pp[:b] + oe).to_slice
+      @env << {ipv0: d[0], ipv1: d[1], ipi: pp[:i], ii0: i[0], ii1: i[1]}
+      @env << {upv0: d[0], upv1: d[1], ui0: i[0], ui1: i[1]}
     end
 
     def set(i : Oid, p : String, o : A)
@@ -177,25 +184,29 @@ module Trove
       decode @env[{di0: i[0], di1: i[1], dp: p}]?.not_nil![:dv] rescue nil
     end
 
+    protected def delete(i : Oid, p : String, ve : String)
+      transaction do |ttx|
+        ttx.env.delete({di0: i[0], di1: i[1], dp: p})
+        pp = partition p
+        dg = digest (pp[:b] + ve).to_slice
+        ttx.env.delete({ipv0: dg[0], ipv1: dg[1], ipi: pp[:i], ii0: i[0], ii1: i[1]})
+        ttx.env.delete({upv0: dg[0], upv1: dg[1]})
+      end
+    end
+
     def delete(i : Oid, p : String = "")
       transaction do |ttx|
         ttx.env.delete({oi0: i[0], oi1: i[1]}) if p.empty?
         ttx.env.from({di0: i[0], di1: i[1], dp: p}) do |d|
-          break unless d[:di0] == i[0] && d[:di1] == i[1] && d[:dp].starts_with? p
-          ttx.env.delete({di0: d[:di0], di1: d[:di1], dp: d[:dp]})
-          iv = index_value d[:dp].bytesize + 16, d[:dv]
-          ttx.env.delete({ip: d[:dp], iv: iv, ii0: d[:di0], ii1: d[:di1]})
-          ttx.env.delete({up: d[:dp], uv: iv})
+          break unless {d[:di0], d[:di1]} == i && d[:dp].starts_with? p
+          delete i, d[:dp], d[:dv]
         end
       end
     end
 
     def delete!(i : Oid, p : String = "")
       transaction do |ttx|
-        v = ttx.env[{di0: i[0], di1: i[1], dp: p}]?.not_nil![:dv] rescue return
-        ttx.env.delete({ip: p, iv: v, ii0: i[0], ii1: i[1]})
-        ttx.env.delete({up: p, uv: v})
-        ttx.env.delete({di0: i[0], di1: i[1], dp: p})
+        delete i, p, (ttx.env[{di0: i[0], di1: i[1], dp: p}]?.not_nil![:dv] rescue return)
       end
     end
 
@@ -220,33 +231,23 @@ module Trove
       end
     end
 
-    protected def index_value(os : Int32, ve : String)
-      if ve.bytesize < 1024
-        ve
-      else
-        d = LibXxhash.xxhash128 ve, ve.size, 0
-        "%016x%016x" % [d.high64, d.low64]
-      end
+    protected def digest(s : Bytes)
+      d = LibXxhash.xxhash128 s, s.size, 0
+      {d.high64, d.low64}
     end
 
-    protected def where(p : String, strict : Bool, v : I, &)
-      iv = index_value p.bytesize + 16, encode v
-      @env.from({ip: p, iv: iv, ii0: 0_u64, ii1: 0_u64}) do |i|
-        break unless i[:iv] == iv && ((strict && i[:ip] == p) || (!strict && i[:ip].starts_with? p))
+    def where(p : String, v : I, &)
+      pp = partition p
+      dg = digest (pp[:b] + encode v).to_slice
+      @env.from({ipv0: dg[0], ipv1: dg[1], ipi: pp[:i], ii0: 0_u64, ii1: 0_u64}) do |i|
+        break unless {i[:ipv0], i[:ipv1]} == dg
         yield({i[:ii0], i[:ii1]})
       end
     end
 
-    def where(p : String, v : I, &)
-      where(p, false, v) { |i| yield i }
-    end
-
-    def where!(p : String, v : I, &)
-      where(p, true, v) { |i| yield i }
-    end
-
     def unique(p : String, v : I)
-      u = @env[{up: p, uv: index_value p.bytesize + 16, encode v}]?
+      dg = digest (p + encode v).to_slice
+      u = @env[{upv0: dg[0], upv1: dg[1]}]?
       return nil unless u
       {u[:ui0], u[:ui1]}
     end
